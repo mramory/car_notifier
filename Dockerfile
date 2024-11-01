2b59d482c608f4d89a78b28f06212281f133c792
mramory <mramor91932765@gmail.com>
2024-11-01 13:55:06 +0300

    new bot, fix dockerfile, add logs

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ WORKDIR /app
 
 # Copy package.json and package-lock.json (if available)
 COPY package.json ./
-COPY package-lock.json ./
+COPY yarn.lock ./
 
 # Install dependencies
 RUN yarn install
@@ -15,7 +15,7 @@ RUN yarn install
 COPY . .
 
 # Build the TypeScript code
-RUN yarn run build
+RUN yarn build
 
 # Install Puppeteer dependencies
 RUN apt-get update && apt-get install -y \
@@ -34,9 +34,10 @@ RUN apt-get update && apt-get install -y \
     libxss1 \
     libxshmfence1 \
     libgbm-dev \
+    libasound2 \
     --no-install-recommends \
     && apt-get clean \
     && rm -rf /var/lib/apt/lists/*
 
 # Command to run your application
-CMD ["npm", "run"]
+CMD ["node", "build/main.js"]
--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,11 @@ import * as process from "node:process";
 const bot = new TelegramBot(process.env.BOT_TOKEN as string);
 
 (async () => {
+    console.log("Started")
     const baseUrl = process.env.AV_BY_URL as string
 
     const browser = await puppeteer.launch({args: ['--no-sandbox']});
+    console.log("Browser launched")
     const page = await browser.newPage();
 
     const scraper = new Scraper(page);
@@ -19,7 +21,7 @@ const bot = new TelegramBot(process.env.BOT_TOKEN as string);
 
     const ids = await scraper.getIds()
     db.insertMany(ids)
-
+    console.log("First iteration successful")
     setInterval(async () => {
         await scraper.goTo(baseUrl)
 
@@ -32,7 +34,7 @@ const bot = new TelegramBot(process.env.BOT_TOKEN as string);
             db.insertMany(newCars)
 
             newCars.map(async (carId) =>
-                await bot.sendMessage("641926170", baseUrl+carId)
+                await bot.sendMessage("7970819815", baseUrl+carId)
             )
         }
         else{
